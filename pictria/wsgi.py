d439bae6240909fb39257ba9b8e45c9ff58f7119
Arshia Malek <arshiama@hotmail.com>
2020-12-22 19:29:32 -0500

    wsgi updated

--- pictria/wsgi.py
+++ pictria/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pictria.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pictria.settings.production')
 
 application = get_wsgi_application()
